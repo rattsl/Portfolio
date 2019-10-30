c6cdca8d91cd2a93be3d73fc9d824d26807287fb
Hayato Kakiuchi <qweaty.uiop8810@i.softbank.jp>
2019-10-30 17:39:36 +0900

    made Top

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,78 @@
   <v-app>
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span class="font-weight-light">PORTFOLIO</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
         text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="#"
         target="_blank"
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">TOP</span>
+      </v-btn>
+      <v-btn
+        text
+        href="#"
+        target="_blank"
+      >
+        <span class="mr-2">ABOUT</span>
+      </v-btn>
+      <v-btn
+        text
+        href="#"
+        target="_blank"
+      >
+        <span class="mr-2">PROFILE</span>
+      </v-btn>
+      <v-btn
+        text
+        href="#"
+        target="_blank"
+      >
+        <span class="mr-2">PORTFOLIO</span>
+        <!-- ここでポップアップでEXPERIENCEとWORKSとSKILLASETTSの3つをだす -->
+      </v-btn>
+      <v-btn
+        text
+        href="#"
+        target="_blank"
+      >
+        <span class="mr-2">CONTACT</span>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <Top/>
+      <About/>
+      <Profile/>
+      <Portfolio/>
+      <contact/>
     </v-content>
+    <v-footer class="indigo">
+    <v-spacer></v-spacer>
+    <div>&copy; {{ new Date().getFullYear() }}</div>
+  </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+
+import Top from '@/containers/Top.vue'
+import About from '@/containers/About.vue'
+import Profile from '@/containers/Profile.vue'
+import Portfolio from '@/containers/Portfolio.vue'
+import Contact from '@/containers/Contact.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Top,
+    About,
+    Profile,
+    Portfolio,
+    Contact,
   },
   data: () => ({
     //
